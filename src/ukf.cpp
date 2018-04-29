684da6925d0323b6bfbc65fedf9a0514b0dae082
scollins83 <saracollins0508@gmail.com>
2018-04-28 20:32:33 -0500

    Added RMSE, and took care of some obvious things in the ukf.cpp to start with.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -12,6 +12,9 @@ using std::vector;
  * This is scaffolding, do not modify
  */
 UKF::UKF() {
+  // Set initialization to false to start.
+  is_initialized_ = false;
+
   // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
 
@@ -25,10 +28,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 5; // Definitely less than 9 for cars.
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = M_PI; // 2pi is too high, so starting by splitting the difference between 2*pi and 0 by just going with pi.
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -49,11 +52,10 @@ UKF::UKF() {
   
   /**
   TODO:
-
   Complete the initialization. See ukf.h for other member properties.
-
-  Hint: one or more values initialized above might be wildly off...
   */
+  P_.setIdentity(); // Initialize P as an identity matrix.
+
 }
 
 UKF::~UKF() {}
@@ -69,6 +71,28 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (!is_initialized_) {
+    // First measurement
+    x_ << 0, 0, 0, 0.5015, 0.3528; // Start with zeros and noise vals from EKF
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      float rho = meas_package.raw_measurements_[0];
+      float phi = meas_package.raw_measurements_[1];
+      float rho_dot = meas_package.raw_measurements_[2];
+
+      // Convert from polar to cartesian
+      x_(0) = rho * cos(phi);
+      x_(1) = rho * sin(phi);
+      x_(2) = rho_dot * cos(phi);
+      x_(3) = rho_dot * cos(phi);
+
+      // TODO: LOOK UP MORE ABOUT 5 X VALUES VS 4!!!!
+
+    }
+
+  }
+
+
+
 }
 
 /**
